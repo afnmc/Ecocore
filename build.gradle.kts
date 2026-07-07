338e4f38cf74ef64b3f0dbceffb8285e93079fcd
Afn <abidfnufail.24@gmail.com>
2026-07-07 09:24:23 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group = "com.azthera"
 version = "1.0.0"
-description = "EcoCore - Survival Economy Engine for Azthera Network"
+description = "EcoCore"
 
 java {
     toolchain {
@@ -16,50 +16,32 @@ java {
 repositories {
     mavenCentral()
 
-    // Paper
     maven("https://repo.papermc.io/repository/maven-public/")
-
-    // PlaceholderAPI
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-
-    // CodeMC
-    maven("https://repo.codemc.org/repository/maven-public/")
-
-    // OpenCollab
-    maven("https://repo.opencollab.dev/main/")
-
-    // JitPack
-    maven("https://jitpack.io")
+    maven("https://repo.codemc.org/repository/maven-releases/")
 }
 
 dependencies {
 
-    // Paper API
     compileOnly("io.papermc.paper:paper-api:1.21.6-R0.1-SNAPSHOT")
 
-    // Vault API
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
+        exclude(group = "org.bukkit")
+    }
 
-    // PlaceholderAPI
     compileOnly("me.clip:placeholderapi:2.11.6")
-
-    // LuckPerms
     compileOnly("net.luckperms:api:5.5")
 
-    // Database
     implementation("com.zaxxer:HikariCP:6.2.1")
     implementation("org.xerial:sqlite-jdbc:3.47.1.0")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.5.1")
     implementation("com.mysql:mysql-connector-j:9.1.0")
 
-    // Adventure
-    compileOnly("net.kyori:adventure-api:4.17.0")
-    compileOnly("net.kyori:adventure-text-minimessage:4.17.0")
+    implementation("net.kyori:adventure-api:4.17.0")
+    implementation("net.kyori:adventure-text-minimessage:4.17.0")
 
-    // Testing
     testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.mockito:mockito-core:5.14.2")
 }
 
 tasks {
@@ -67,18 +49,12 @@ tasks {
     compileJava {
         options.encoding = "UTF-8"
         options.release.set(25)
-        options.compilerArgs.addAll(
-            listOf(
-                "-Xlint:all",
-                "-parameters"
-            )
-        )
     }
 
     processResources {
 
         val props = mapOf(
-            "version" to version
+            "version" to project.version
         )
 
         inputs.properties(props)
@@ -88,14 +64,9 @@ tasks {
         }
     }
 
-    test {
-        useJUnitPlatform()
-    }
-
     shadowJar {
 
         archiveClassifier.set("")
-        archiveBaseName.set("EcoCore")
 
         relocate(
             "com.zaxxer.hikari",
@@ -117,22 +88,14 @@ tasks {
             "com.azthera.ecocore.libs.mysql"
         )
 
-        minimize {
-
-            exclude(dependency("com.zaxxer:HikariCP:.*"))
-            exclude(dependency("org.xerial:sqlite-jdbc:.*"))
-            exclude(dependency("org.mariadb.jdbc:mariadb-java-client:.*"))
-            exclude(dependency("com.mysql:mysql-connector-j:.*"))
-
-        }
-
+        minimize()
     }
 
     build {
         dependsOn(shadowJar)
     }
 
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
+    test {
+        useJUnitPlatform()
     }
 }
